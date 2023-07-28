0f0de2fb741f30bef15ca3cf03e006255a9dc210
luccaneivas <luccaneiva12@gmail.com>
2023-07-28 00:07:12 -0300

    Moving pieces

--- java_chess/src/application/Program.java
+++ java_chess/src/application/Program.java
@@ -1,10 +1,42 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.exceptions.ChessException;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World;");
+		Scanner sc = new Scanner(System.in);
+		ChessMatch match = new ChessMatch();
+
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(match.getPieces());
+
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedPiece = match.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
 	}
 
 }
--- java_chess/src/chess/ChessMatch.java
+++ java_chess/src/chess/ChessMatch.java
@@ -1,43 +1,79 @@
 package chess;
 
 import boardgame.Board;
-
+import boardgame.Piece;
+import boardgame.Position;
+import chess.exceptions.ChessException;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
 
 	private Board board;
-	
+
 	public ChessMatch() {
-		this.board = new Board(8,8);
+		this.board = new Board(8, 8);
 		initialSetup();
 	}
-	
+
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] pieces = new ChessPiece[board.getRows()][board.getColumns()];
-		
+
 		for (int i = 0; i < board.getColumns(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
-				pieces[i][j] = (ChessPiece) board.piece(i,j); // desse jeito você força a peça a ser do tipo ChessPiece, que herda a classe Piece.
+				pieces[i][j] = (ChessPiece) board.piece(i, j); // desse jeito você força a peça a ser do tipo
+																// ChessPiece, que herda a classe Piece.
 			}
 		}
-		
+
 		return pieces;
 	}
-	
+
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+
+		validateSourcePosition(source);
+
+		Piece capturedPiece = makeMove(source, target);
+
+		return (ChessPiece) capturedPiece;
+	}
+
+	private Piece makeMove(Position source, Position target) {
+		Piece piece = board.removePiece(source);
+
+		Piece capturedPiece = board.removePiece(target);
+
+		board.placePiece(piece, target);
+
+		return capturedPiece;
+	}
+
+	private void validateSourcePosition(Position position) {
+		if (!board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
+		}
+	}
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
+
 	private void initialSetup() {
-		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.WHITE));
-		
-		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
-		placeNewPiece('h', 1, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.BLACK));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
-	
+
 }
